1700e780692d616af25b7039f17204ea3cc5895e
kutty selva <kuttyselva57@gmail.com>
2018-06-18 14:52:57 +0530

    initial

---
+++ pro.js
@@ -0,0 +1,17 @@
+//alert("welcome to your bank");
+//var fn= prompt("enter the frst name");
+//var ln= prompt("enter the lastname");
+//var age= prompt("enter the age");
+//var len= prompt("enter the hi8");
+//if((fn[0]==ln[0])&&(age>20)&&(age<40)&&(len>150))
+//{console.log("you are a spy");}
+
+function sleepin(weekday,vacation) {
+  if(weekday=="true")
+  {
+    console.log("yfalse");
+  }
+  else {
+    console.log("true");
+  }
+}
