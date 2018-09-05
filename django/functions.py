92519aa7b8e187efac822fe496c76ce29f7f95d3
kuttyselva <kuttyselva57@gmail.com>
2018-09-05 22:55:07 +0530

    lamda

--- django/functions.py
+++ django/functions.py
@@ -1,3 +1,13 @@
 def myfun(par):
         print("frst call{}".format(par));
-myfun('default')
+        return("hii")
+xval=myfun('default')
+print (xval)
+
+mylist=[1,2,3,4,5,6,7,8,9]
+def checkeve(num):
+    return num%2 ==0
+res=filter(checkeve,mylist)
+print(res)
+defs=filter(lambda num:num%2==0,mylist)
+print(defs)
