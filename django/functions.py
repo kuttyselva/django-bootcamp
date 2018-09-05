6ac4b87097a55208558b01080d9c2ea22da9696c
kuttyselva <kuttyselva57@gmail.com>
2018-09-05 22:41:01 +0530

    mui

---
+++ django/functions.py
@@ -0,0 +1,3 @@
+def myfun(par):
+        print("frst call{}".format(par));
+myfun('default')
--- django/strings.py
+++ django/strings.py
@@ -1,3 +1,8 @@
 print ("hello")
 x= "item 1 is {x} two is {y}".format(x="a",y="b");
 print (x);
+
+s= "django"
+print(s[0]);
+print(s[1:4]);
+print(s[::-1]);
